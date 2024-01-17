0f755ffc92cb4228a8182d994878c1268315bd24
Esmat Farahi <esmatullah.farahi@gmail.com>
2024-01-17 15:57:48 -0500

    fetch data

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,8 @@
 import  express  from "express";
 import products from "./data/products.js"
-const port =5000;
+dotenv.config();
+import dotenv from "dotenv"
+const port =process.env.PORT || 5000;
 
 
 
@@ -8,12 +10,15 @@ const app = express();
 
 
 app.get('/',(req,res)=>{
-    res.end("Api is running...");
+    res.send("Api is running...");
 });
 
+app.get("/api/products", (req,res) =>{
+    res.json(products);
+})
 app.get("/api/products/:id",(req, res)=> {
     const product = products.find((p)=> p._id === req.params.id);
     res.json(product);
-})
+});
 
 app.listen(port, () =>console.log(`Server is running on port ${port} `));
